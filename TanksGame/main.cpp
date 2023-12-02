ce98b8bd4a08906a6a47c41e480d62ac859d4c14
LevkoBe <lbeniakh@kse.org.ua>
2023-12-02 21:22:17 +0200

    Some ground for game logic

--- TanksGame/main.cpp
+++ TanksGame/main.cpp
@@ -1,88 +1,182 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Text and Image Display");
-    sf::Font font;
-    font.loadFromFile("Arial.ttf"); // Change the font file path as needed
+enum ProjectileType {
+    CannonBall, // basic
+    FireBall,   // burns
+    SnowBall,   // slows down
+    Chip,       // turns into an ally
+    Magnet,     // attracts to itself
+    LazerBeam,  // basically basic
+    Reducer     // makes smaller
+};
 
-    sf::Text text;
-    text.setFont(font);
-    text.setCharacterSize(30);
-    text.setFillColor(sf::Color::White);
+class Tank {
+private:
+    int vMove;    // move speed
+    int vShoot;   // fire speed
+    int vReload;  // time of reloading
+    int pHealth;  // health points
+    int pDamage;  // strength of pDamage
+    int pxSize;   // size in pixels
+    ProjectileType projectile;
 
-    std::string userInput;
+public:
+    Tank(int coefficient = 100) :
+        vMove(coefficient / 100 * 10), vShoot(coefficient / 100 * 20), vReload(coefficient / 100 * 10), pHealth(coefficient / 100 * 100), pDamage(coefficient / 100 * 10), pxSize(coefficient / 100 * 100), projectile(CannonBall) {};
+    Tank(int vMove, int vShoot, int vReload, int pHealth, int pDamage, int size, ProjectileType projectile) :
+        vMove(vMove), vShoot(vShoot), vReload(vReload), pHealth(pHealth), pDamage(pDamage), pxSize(size), projectile(projectile) {};
+    void move(int x, int y) {};
+    void shoot() {};
+    void setProjectileType(ProjectileType projectileType) {};
+};
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("./images/tank1.png")) { // Change the image file path as needed
-        std::cerr << "Failed to load image!" << std::endl;
-        return 1;
-    }
-
-    sf::Sprite sprite(texture);
-    sprite.setPosition(200, 200); // Adjust the position of the image
-
-    // Set the origin to the center of the image
-    sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
-
+class Renderer {
+private:
+    const int windowSize = 200;
     float rotationAngle = 0.0f;
     const float rotationSpeed = 5.0f;
     const float movementSpeed = 5.0f;
+    sf::RenderWindow window;
+    sf::Font font;
+    sf::Text text;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    std::string userInput = "";
 
-    while (window.isOpen()) {
+public:
+    Renderer() : window(sf::VideoMode(800, 600), "Tank Game") {
+        font.loadFromFile("Arial.ttf");
+        text.setFont(font);
+        text.setCharacterSize(30);
+        text.setFillColor(sf::Color::White);
+        texture.loadFromFile("./images/tank1.png");
+        sprite = sf::Sprite(texture);
+        sprite.setPosition(200, 200);
+        sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+    }
+
+    void run() {
+        initWindow();
+        while (window.isOpen()) {
+            processEvents();
+            update();
+            render();
+        }
+    }
+
+    void initWindow() {
+        // Initialize window properties
+    }
+
+    void update() {
+        // Update game logic here if needed
+    }
+
+    void render() {
+        window.clear();
+        window.draw(text);
+        window.draw(sprite); // Draw the tank
+        window.display();
+    }
+
+    void processEvents() {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
             else if (event.type == sf::Event::TextEntered) {
-                if (event.text.unicode < 128) {
-                    if (event.text.unicode == '\b' && !userInput.empty()) { // Backspace
-                        userInput.pop_back();
-                    }
-                    else if ((event.text.unicode == '\r' || event.text.unicode == '\n')) { // Enter
-                        userInput += "\n";
-                    }
-                    else if (event.text.unicode == 127) { // Delete
-                        userInput.clear();
-                    }
-                    else {
-                        userInput += static_cast<char>(event.text.unicode);
-                    }
-
-                    text.setString(userInput);
-                }
+                handleTextEvent(event);
             }
             else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Subtract) { // Decrease image size
-                    sprite.setScale(sprite.getScale() * 0.9f);
-                }
-                else if (event.key.code == sf::Keyboard::Add) { // Increase image size
-                    sprite.setScale(sprite.getScale() * 1.1f);
-                }
-                else if (event.key.code == sf::Keyboard::Left) { // Rotate image left
-                    rotationAngle -= rotationSpeed;
-                    sprite.setRotation(rotationAngle);
-                }
-                else if (event.key.code == sf::Keyboard::Right) { // Rotate image right
-                    rotationAngle += rotationSpeed;
-                    sprite.setRotation(rotationAngle);
-                }
-                else if (event.key.code == sf::Keyboard::Up) { // Move image forward
-                    float radians = (rotationAngle - 90) * (3.14159265f / 180.0f);
-                    float moveX = movementSpeed * std::cos(radians);
-                    float moveY = movementSpeed * std::sin(radians);
-                    sprite.move(moveX, moveY);
-                }
+                handleKeyEvent(event);
             }
         }
-
-        window.clear();
-        window.draw(sprite); // Draw the image
-        window.draw(text);
-        window.display();
     }
+
+private:
+
+    void handleTextEvent(sf::Event& event) {
+        if (event.text.unicode < 128) {
+            if (event.text.unicode == '\b' && !userInput.empty()) { // Backspace
+                userInput.pop_back();
+            }
+            else if ((event.text.unicode == '\r' || event.text.unicode == '\n')) { // Enter
+                userInput += "\n";
+            }
+            else if (event.text.unicode == 127) { // Delete
+                userInput.clear();
+            }
+            else {
+                userInput += static_cast<char>(event.text.unicode);
+            }
+
+            text.setString(userInput);
+        }
+    }
+
+    void handleKeyEvent(sf::Event& event) {
+        if (event.key.code == sf::Keyboard::Subtract) { // Decrease image size
+            sprite.setScale(sprite.getScale() * 0.9f);
+        }
+        else if (event.key.code == sf::Keyboard::Add) { // Increase image size
+            sprite.setScale(sprite.getScale() * 1.1f);
+        }
+        else if (event.key.code == sf::Keyboard::Left) { // Rotate image left
+            rotationAngle -= rotationSpeed;
+            sprite.setRotation(rotationAngle);
+        }
+        else if (event.key.code == sf::Keyboard::Right) { // Rotate image right
+            rotationAngle += rotationSpeed;
+            sprite.setRotation(rotationAngle);
+        }
+        else if (event.key.code == sf::Keyboard::Up) { // Move image forward
+            moveTankForward();
+        }
+    }
+
+    void moveTankForward() {
+        float radians = (rotationAngle - 90) * (3.14159265f / 180.0f);
+        float moveX = movementSpeed * std::cos(radians);
+        float moveY = movementSpeed * std::sin(radians);
+        sprite.move(moveX, moveY);
+    }
+};
+
+class GameRun {
+private:
+    int gridSize;
+    int level;
+    std::vector<std::vector<bool>> map;
+    Tank* user;
+    std::vector<Tank> bots;
+    std::unique_ptr<Renderer> renderer;  // Use smart pointer for ownership
+
+public:
+    GameRun() : renderer(std::make_unique<Renderer>()) {
+        createMap();
+    }
+
+    void createMap() {
+        // creating an array;
+    }
+
+    void reset() {}
+    void makeMove(int x, int y) {}
+
+    void run() {
+        renderer->run();
+    }
+
+};
+
+
+int main() {
+    GameRun game;
+    game.run();
 
     return 0;
 }
